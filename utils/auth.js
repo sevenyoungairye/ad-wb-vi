3ebecdbaca811f59dab1dcadb2d8168b76428083
sevenyoungairye <lel.ng.top@gmail.com>
2022-01-09 11:34:58 +0800

    sub

--- utils/auth.js
+++ utils/auth.js
@@ -8,7 +8,7 @@ export function getToken() {
 
 export function setToken(token) {
   return Cookies.set(TokenKey, token, {
-    expires: new Date(new Date().getTime + 2 * 24 * 60 * 60 * 1000)
+    expires: new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000)
   })
 }
 
