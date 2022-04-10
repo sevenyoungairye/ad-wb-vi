119d9d7324d5cc76b4e81c51ee724fb6bb02d050
sevenyoungairye <lel.ng.top@gmail.com>
2022-04-10 13:57:09 +0800

    工具栏，小编推荐。

---
+++ pages/ext/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="ext-bg">
+    <div>
+      <el-collapse accordion>
+        <el-collapse-item title="一个好玩的播放插件">
+          <div class="text-desc">
+            <el-link u:underline="false" target="_blank" href="/parse/play">
+              🎈链接直达
+            </el-link>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+
+      <div class="hide-div"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "extend page",
+    };
+  },
+  data() {
+    return {};
+  },
+  created() {},
+};
+</script>
+
+<style>
+.text-desc {
+  margin-left: 5px;
+}
+
+
+.ext-bg {
+  background: url("https://w.wallhaven.cc/full/y8/wallhaven-y8lkdd.jpg");
+  opacity: 100%;
+  margin-top: 1px;
+  width: 100%;
+  height: 100%;
+  min-width: 1000px;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+}
+
+.hide-div {
+  /* height: 64.6vh; */
+  height: 100vh;
+}
+
+.el-collapse-item__header {
+  opacity: 90%;
+  color: #999;
+  font-weight: bolder;
+}
+
+.el-collapse-item__wrap {
+  opacity: 60%;
+}
+</style>
---
+++ pages/ext/parse.vue
@@ -0,0 +1,195 @@
+<!-- a temp parse m3u8 page -->
+<template>
+  <div>
+    <!-- 
+      https://aidi.tv/play/1931-1-1.html
+       -->
+    <div>
+      <el-collapse accordion>
+        <el-collapse-item title="播放操作">
+          <div>
+            <el-input
+              v-show="showInput"
+              placeholder="输入要播放的链接"
+              v-model="playUrl"
+              clearable
+            />
+            <el-button @click="doPlay">{{
+              showInput ? "播放" : "搜索"
+            }}</el-button>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item title="小编推荐">
+          <el-descriptions
+            class="margin-top"
+            title="播放链接推荐~"
+            :column="1"
+            size="medium"
+          >
+            <el-descriptions-item
+              v-for="(item, index) in dataList"
+              :key="index"
+              :label="item.filmDesc"
+            >
+              <el-tooltip content="点击复制" placement="bottom" effect="light">
+                <el-tag
+                  @click="copyText($event)"
+                  v-for="(url, j) in item.m3u8List"
+                  :key="j"
+                >
+                  {{ url }}
+                </el-tag>
+              </el-tooltip>
+            </el-descriptions-item>
+          </el-descriptions>
+
+          <div style="text-align: center">
+            <el-tooltip :content="loadMoreTip">
+              <el-button class="load-more-btn" @click="loadMore" type="text">
+                ...
+              </el-button>
+            </el-tooltip>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item title="tips">
+          <el-row>
+            <el-col>
+              有些资源存在跨域问题, 播放不了, 请使用Microsoft Edge +
+              插件配合食用。
+            </el-col>
+          </el-row>
+
+          <el-row>
+            <el-col>
+              <el-tag>
+                <el-tooltip
+                  content="点击跳转"
+                  placement="bottom"
+                  effect="light"
+                >
+                  <el-link
+                    :underline="false"
+                    target="_blank"
+                    href="https://microsoftedge.microsoft.com/addons/detail/allow-cors-accesscontro/bhjepjpgngghppolkjdhckmnfphffdag?hl=zh-CN"
+                  >
+                    去下载插件
+                  </el-link>
+                </el-tooltip>
+              </el-tag>
+            </el-col>
+          </el-row>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+
+    <BasePlayer v-show="showPlayer" v-bind:playObj="playObj" />
+  </div>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+import { doCopy, arrayDiff } from "@/utils/common/tool.js";
+
+export default {
+  head() {
+    return {
+      title: "parse api",
+    };
+  },
+  components: {},
+  props: {},
+  data() {
+    //这里存放数据
+    return {
+      loadMoreTip: "点击加载更多",
+      totalPage: 0,
+      dataList: [],
+      queryParams: {
+        params: {
+          page: 1,
+          limit: 1,
+        },
+      },
+      playUrl: undefined,
+      showInput: true,
+      showPlayer: false,
+      playObj: {
+        videoId: null,
+        playUrl: null,
+        mode: "auto",
+        title: "api parse page",
+      },
+    };
+  },
+  computed: {},
+  watch: {
+    "queryParams.params.page": {
+      handler(val) {
+        if (val === this.totalPage) {
+          this.loadMoreTip = "没有更多内容了...";
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    loadMore() {
+      if (this.queryParams.params.page < this.totalPage) {
+        ++this.queryParams.params.page;
+        let _dataList = [...this.dataList];
+        this.getData().then((dataList) => {
+          dataList.forEach((item) => {
+            _dataList.push(item);
+          });
+          console.log(_dataList);
+          this.dataList = arrayDiff(_dataList, "filmDesc");
+        });
+      }
+    },
+    getData() {
+      return this.$api
+        .get("/v1/view/tool/parse/l", this.queryParams)
+        .then((resp) => resp.data)
+        .then((resp) => {
+          if (resp.success) {
+            this.totalPage = resp.data.totalPage;
+            this.dataList = resp.data.list;
+            return this.dataList;
+          }
+          return new Array();
+        })
+        .catch((e) => console.log(e));
+    },
+    copyText(e) {
+      doCopy(e.target);
+    },
+    doPlay() {
+      this.showInput = !this.showInput;
+      if (this.playUrl && this.playUrl.trim() !== "") {
+        this.showPlayer = true;
+        this.$nextTick(() => {
+          this.playObj.playUrl = this.playUrl;
+        });
+      } else {
+        this.showPlayer = false;
+      }
+    },
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    this.getData();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+};
+</script>
+<style scoped>
+.load-more-btn {
+  font-weight: bolder;
+  font-size: 30px;
+  color: #c0c4cc;
+}
+</style>
